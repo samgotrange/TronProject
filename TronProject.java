11e83424ebc6b195a6a360c16a62c1ef196def4d
Colem18 <cole2018@gmail.com>
2017-05-23 12:03:59 -0700

    Add files via upload

---
+++ TronProject.java
@@ -0,0 +1,8 @@
+package tronproject;
+public class TronProject {
+    public static void main(String[] args) {
+        TronGame game = new TronGame();
+        game.setVisible(true);
+    }
+    
+}
