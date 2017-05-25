0534e4391320e0835989b49a10c8acad2203f54c
Colem18 <cole2018@gmail.com>
2017-05-25 11:54:47 -0700

    Update TronProject.java

--- TronProject.java
+++ TronProject.java
@@ -3,6 +3,8 @@ public class TronProject {
     public static void main(String[] args) {
         TronGame game = new TronGame();
         game.setVisible(true);
+        game.setSize(1000,750);
+        game.setLocation(150,100);
     }
     
 }
