022eaa6d465d1f3fffea45087a5a6746c0772ee6
Colem18 <cole2018@gmail.com>
2017-05-26 11:50:32 -0700

    Update TronGame.java

--- TronGame.java
+++ TronGame.java
@@ -1,19 +1,5 @@
 package tronproject;
 import java.awt.*;
-import java.awt.event.*;
-public class TronGame extends Frame implements KeyListener {
-    @Override
-    public void keyTyped(KeyEvent ke) {
-       
-    }
-
-    @Override
-    public void keyPressed(KeyEvent ke) {
-        
-    }
-
-    @Override
-    public void keyReleased(KeyEvent ke) {
-        
-    }
+public class TronGame extends Frame {
+   
 }
