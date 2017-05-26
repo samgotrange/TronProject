7c22cf9f9b581c0aac3dcc573003286b8a413f1a
Colem18 <cole2018@gmail.com>
2017-05-26 11:52:18 -0700

    Add files via upload

---
+++ Player2.java
@@ -0,0 +1,23 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class Player2 implements KeyListener{
+    private int xPos;
+    private int yPos;
+    private int velo;
+    private int direction;
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        
+    }
+}
