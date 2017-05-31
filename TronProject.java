3cbda936f9c69f99d80ca9136f8c6fc1a16dcd91
Colem18 <cole2018@gmail.com>
2017-05-31 12:20:29 -0700

    Update TronProject.java

--- TronProject.java
+++ TronProject.java
@@ -2,9 +2,9 @@ package tronproject;
 public class TronProject {
     public static void main(String[] args) {
         TronGame game = new TronGame();
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         game.setVisible(true);
         game.setSize(9999,9999);
-        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
