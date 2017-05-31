29f926c6b83ba6e2c71d4ae939601d68d3f8a7b2
Colem18 <cole2018@gmail.com>
2017-05-31 11:40:57 -0700

    Update Player1.java

--- Player1.java
+++ Player1.java
@@ -5,22 +5,4 @@ public class Player1 implements KeyListener {
     private int yPos;
     private int velo;
     private int direction = 2;
-
-    public Player1(){
-        this.addKeyListener(this);
-    }
-    @Override
-    public void keyTyped(KeyEvent ke) {
-        
-    }
-
-    @Override
-    public void keyPressed(KeyEvent ke) {
-        
-    }
-
-    @Override
-    public void keyReleased(KeyEvent ke) {
-        
-    }
 }
