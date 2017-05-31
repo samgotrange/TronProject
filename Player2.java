3b49f4c74dbecd6b4ad916edd685902e804587dc
Colem18 <cole2018@gmail.com>
2017-05-31 11:43:49 -0700

    Update Player2.java

--- Player2.java
+++ Player2.java
@@ -1,23 +1,10 @@
 package tronproject;
 import java.awt.event.*;
-public class Player2 implements KeyListener{
+public class Player2{
     private int xPos;
     private int yPos;
     private int velo;
     private int direction = 4;
 
-    @Override
-    public void keyTyped(KeyEvent ke) {
-        
-    }
 
-    @Override
-    public void keyPressed(KeyEvent ke) {
-        
-    }
-
-    @Override
-    public void keyReleased(KeyEvent ke) {
-        
-    }
 }
