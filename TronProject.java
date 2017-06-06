a77893d5d63d604ef5782c7b4751537ce7690f8d
Colem18 <cole2018@gmail.com>
2017-06-06 10:27:26 -0700

    Update TronProject.java

--- TronProject.java
+++ TronProject.java
@@ -1,10 +1,18 @@
 package tronproject;
+
+import javax.swing.JFrame;
+
 public class TronProject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        boolean gameOn = true;
         TronGame game = new TronGame();
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         game.setVisible(true);
         game.setSize(9999,9999);
+        while (gameOn == true) {
+            game.player1.move1();
+            game.player2.move2();
+        }
     }
     
 }
