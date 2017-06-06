e98fb255a7dfbbaea917ce35d54bd4d4f22421dd
Colem18 <cole2018@gmail.com>
2017-06-06 10:28:22 -0700

    Update TronGame.java

--- TronGame.java
+++ TronGame.java
@@ -7,9 +7,23 @@ public class TronGame extends JFrame implements KeyListener {
     Player2 player2 = new Player2();
     @Override
     public void keyTyped(KeyEvent ke) {
-        if (ke.getKeyChar()== ){
-            
-    }
+        if (ke.getKeyChar()== 37){
+            player2.setDirection(1);
+        }else if (ke.getKeyChar()== 38){
+            player2.setDirection(2);
+        }else if (ke.getKeyChar()== 39){
+            player2.setDirection(3);
+        }else if (ke.getKeyChar()== 40){
+            player2.setDirection(4);
+        }else if (ke.getKeyChar()== 65){
+            player1.setDirection(1);
+        }else if (ke.getKeyChar()== 87){
+            player1.setDirection(2);
+        }else if (ke.getKeyChar()== 68){
+            player1.setDirection(3);
+        }else if (ke.getKeyChar()== 83){
+            player1.setDirection(4);
+        }
     }
 
     @Override
