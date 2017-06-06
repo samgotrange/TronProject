997eb0804cf38120b959122d37e521feb3a21b8b
Colem18 <cole2018@gmail.com>
2017-06-06 10:28:50 -0700

    Update Player2.java

--- Player2.java
+++ Player2.java
@@ -1,10 +1,31 @@
 package tronproject;
 import java.awt.event.*;
+import java.util.concurrent.TimeUnit;
 public class Player2{
     private int xPos;
     private int yPos;
-    private int velo;
-    private int direction = 4;
-
-
+    private int velo = 10;
+    private int direction = 1;
+       
+    public int getDirection1() {
+        return direction;
+    }
+    public void setDirection(int a) {
+        direction = a;
+    }
+    public void move2() throws InterruptedException {
+        if (direction == 1) {
+            xPos -= velo;
+            TimeUnit.MILLISECONDS.sleep(100);
+        }else if (direction == 2) {
+            yPos -= velo;
+            TimeUnit.MILLISECONDS.sleep(100);
+        }else if (direction == 3) {
+            xPos += velo;
+            TimeUnit.MILLISECONDS.sleep(100);
+        }else if (direction == 4) {
+            yPos += velo;
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
+    }
 }
