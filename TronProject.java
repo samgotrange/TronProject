cda0991f5c6b70c37ef3207465abc6c654059484
samgotrange <samr.range@gmail.com>
2017-06-08 09:59:06 -0700

    Create TronProject.java

--- TronProject.java
+++ TronProject.java
@@ -1,18 +1,26 @@
 package tronproject;
 
+import java.awt.*;
 import javax.swing.JFrame;
 
 public class TronProject {
+
     public static void main(String[] args) throws InterruptedException {
         boolean gameOn = true;
+        jframe j = new jframe();
+        j.setSize(9999, 9999);
+        j.setVisible(true);
         TronGame game = new TronGame();
-        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         game.setVisible(true);
-        game.setSize(9999,9999);
+        game.setLocation(0,0);
+        j.add(game);
+        Graphics g = game.getGraphics();
         while (gameOn == true) {
             game.player1.move1();
+            g.fillRect(game.player1.getxpos(), game.player1.getypos(), 5, 5);
             game.player2.move2();
+            g.fillRect(game.player2.getxpos(), game.player2.getypos(), 5, 5);
         }
     }
-    
+
 }
