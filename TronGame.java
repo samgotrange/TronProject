fcf9c593e6b81d368e49fe144b40cf359fa105ed
Colem18 <cole2018@gmail.com>
2017-06-13 11:09:52 -0700

    Update TronGame.java

--- TronGame.java
+++ TronGame.java
@@ -12,21 +12,19 @@ public class TronGame extends JPanel implements KeyListener, ActionListener {
     Player1 player1;
     Player2 player2;
     private Timer timer;
-    
-    
+
     public TronGame() {
         player1 = new Player1();
         player2 = new Player2();
-        //KeyListener k = new KeyListener();
+        addKeyListener(this);
     }
-    
-    public void Start(){
+
+    public void Start() {
         timer = new Timer(25, this);
         timer.start();
     }
-    
-    
-    public void actionPerformed (ActionEvent ae) {
+
+    public void actionPerformed(ActionEvent ae) {
         try {
             player1.move1();
         } catch (InterruptedException ex) {
@@ -39,19 +37,24 @@ public class TronGame extends JPanel implements KeyListener, ActionListener {
         }
         this.repaint();
     }
-    
+
     // method for overriding paintComponent
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.fillRect(player1.getxpos(), player1.getypos(), 10, 10);
         g.fillRect(player2.getxpos(), player2.getypos(), 10, 10);
-                
+
     }
-    
+
     @Override
     public void keyTyped(KeyEvent ke) {
 
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        System.out.println(ke.getKeyChar());
         if (ke.getKeyChar() == 37) {
             player2.setDirection(1);
         } else if (ke.getKeyChar() == 38) {
@@ -72,16 +75,9 @@ public class TronGame extends JPanel implements KeyListener, ActionListener {
     }
 
     @Override
-    public void keyPressed(KeyEvent ke) {
-
-    }
-
-    @Override
     public void keyReleased(KeyEvent ke) {
 
     }
-
-    // timer event / method
-    
-
 }
+    // timer event / method
+
